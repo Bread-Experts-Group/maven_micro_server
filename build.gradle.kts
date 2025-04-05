fdd26c6100e8b467220a6a34ce079697f51e18dd
ATPStorages <tcgytsc@gmail.com>
2025-04-05 01:57:31 -0500

    Maven micro server.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import java.util.Properties
+
 plugins {
 	kotlin("jvm") version "2.1.10"
 	`maven-publish`
+	signing
+	application
 }
 
 group = "bread_experts_group"
@@ -8,49 +12,86 @@ version = "1.0-SNAPSHOT"
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://maven.javart.zip/") }
 }
 
 dependencies {
 	testImplementation(kotlin("test"))
-	implementation(files("C:\\Users\\Adenosine3Phosphate\\Desktop\\Projects\\bread_server_lib\\build\\libs\\bread_server_lib-1.0-SNAPSHOT.jar"))
+	implementation(kotlin("reflect"))
+	implementation("bread_experts_group:bread_server_lib-code:1.0-SNAPSHOT")
 }
 
 tasks.test {
 	useJUnitPlatform()
 }
+application {
+	mainClass = "bread_experts_group.MainKt"
+}
+
+java {
+	withJavadocJar()
+	withSourcesJar()
+}
 kotlin {
 	jvmToolchain(21)
 }
+val localProperties = Properties().apply {
+	rootProject.file("local.properties").reader().use(::load)
+}
 publishing {
 	publications {
-		create<MavenPublication>("mavenJava") {
+		create<MavenPublication>("mavenKotlinDist") {
+			artifact(tasks.distZip)
+			artifact(tasks.distTar)
+		}
+		create<MavenPublication>("mavenKotlin") {
+			artifactId = "$artifactId-code"
+			from(components["kotlin"])
 			pom {
-				name = "My Library"
-				description = "A concise description of my library"
-				url = "http://www.example.com/library"
-				properties = mapOf(
-					"myProp" to "value",
-					"prop.with.dots" to "anotherValue"
-				)
+				name = "Maven file server"
+				description = "Distribution of software for Bread Experts Group Maven file servers."
+				url = "https://javart.zip"
+				signing {
+					sign(publishing.publications["mavenKotlin"])
+					sign(configurations.archives.get())
+				}
 				licenses {
 					license {
-						name = "The Apache License, Version 2.0"
-						url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+						name = "GNU General Public License v3.0"
+						url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
 					}
 				}
 				developers {
 					developer {
-						id = "johnd"
-						name = "John Doe"
-						email = "john.doe@example.com"
+						id = "mikoe"
+						name = "Miko Elbrecht"
+						email = "miko@javart.zip"
 					}
 				}
 				scm {
-					connection = "scm:git:git://example.com/my-library.git"
-					developerConnection = "scm:git:ssh://example.com/my-library.git"
-					url = "http://example.com/my-library/"
+					connection = "scm:git:git://github.com/Bread-Experts-Group/bread_server_lib.git"
+					developerConnection = "scm:git:ssh://git@github.com:Bread-Experts-Group/maven_micro_server.git"
+					url = "https://javart.zip"
 				}
 			}
 		}
+	}
+	repositories {
+		maven {
+			url = uri("https://maven.javart.zip/")
+			credentials {
+				username = localProperties["mavenUser"] as String
+				password = localProperties["mavenPassword"] as String
+			}
+		}
+	}
+}
+signing {
+	useGpgCmd()
+	sign(publishing.publications["mavenKotlin"])
+}
+tasks.javadoc {
+	if (JavaVersion.current().isJava9Compatible) {
+		(options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
 	}
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,135 @@
 package bread_experts_group
 
+import bread_experts_group.http.HTTPMethod
+import bread_experts_group.http.HTTPRequest
+import bread_experts_group.http.HTTPResponse
+import java.io.File
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.ServerSocket
+import javax.net.ssl.SSLException
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.math.min
+
+@OptIn(ExperimentalEncodingApi::class)
 fun main(args: Array<String>) {
+	Thread.currentThread().name = "Maven-Main"
+	debug("- Argument read")
 	val (singleArgs, multipleArgs) = readArgs(
-		args
+		args,
+		Flag<String>("ip"),
+		Flag<Int>("port", default = 443, conv = ::stringToInt),
+		Flag<Int>("verbosity", default = 1, conv = ::stringToInt),
+		Flag<String>("store", repeatable = true),
+		Flag<String>("credential", repeatable = true),
 	)
-	debug("Hello World!")
-	info("Hello World!")
-	warn("Hello World!")
-	error("Hello World!")
+	toStringVerbosity = (singleArgs["verbosity"] as? Int) ?: toStringVerbosity
+	debug("- Socket retrieval")
+	val serverSocket = ServerSocket()
+	debug("- Socket bind")
+	serverSocket.bind(
+		InetSocketAddress(
+			(singleArgs["ip"] as? String) ?: "0.0.0.0",
+			singleArgs["port"] as Int
+		)
+	)
+	info("- Server loop")
+	val credentialTable = multipleArgs.getValue("credential").associate {
+		val credential = (it as String).split(',')
+		credential[0] to credential[1]
+	}
+	val mavenStores = multipleArgs.getValue("store").map { File(it as String) }
+	while (true) {
+		val sock = serverSocket.accept()
+		Thread.ofVirtual().name("Maven-${sock.localPort}<${sock.inetAddress}").start {
+			try {
+				val request = HTTPRequest.read(sock.inputStream)
+				val storePath = request.path.substring(1)
+				when (request.method) {
+					HTTPMethod.GET, HTTPMethod.HEAD -> mavenStores.firstOrNull {
+						val requestedPath = it.resolve(storePath)
+						if (requestedPath.exists()) {
+							debug("Found file for \"$storePath\" at \"${requestedPath.absolutePath}\"")
+							HTTPResponse(
+								200, request.version,
+								mapOf(
+									"Content-Type" to "application/octet-stream",
+									"Content-Length" to requestedPath.length().toString()
+								)
+							).write(sock.outputStream)
+							if (request.method == HTTPMethod.GET)
+								requestedPath.inputStream().transferTo(sock.outputStream)
+							true
+						} else false
+					} ?: run {
+						warn("No found file for \"$storePath\"")
+						HTTPResponse(404, request.version)
+							.write(sock.outputStream)
+					}
+
+					HTTPMethod.PUT -> {
+						if (credentialTable.isNotEmpty()) {
+							val authorization = request.headers["Authorization"]
+							if (authorization == null) {
+								error("No user provided, unauthorized for PUT")
+								HTTPResponse(401, request.version)
+									.write(sock.outputStream)
+								return@start
+							}
+							val pair = Base64.decode(authorization.substringAfter("Basic "))
+								.decodeToString()
+								.split(':')
+							val password = credentialTable[pair[0]]
+							if (password == null || password != pair[1]) {
+								error("${pair[0]} unauthorized for PUT, not a user or wrong password")
+								HTTPResponse(403, request.version)
+									.write(sock.outputStream)
+								return@start
+							}
+							info("${pair[0]} authorized. Hello")
+						}
+
+						val size = request.headers["Content-Length"]?.toLongOrNull()
+						if (size == null || size < 1) {
+							warn("No size specified for PUT.")
+							HTTPResponse(400, request.version)
+								.write(sock.outputStream)
+						} else {
+							var writtenFile: File? = null
+							mavenStores.forEach {
+								val requestedPath = it.resolve(storePath)
+								requestedPath.deleteRecursively()
+								requestedPath.parentFile.mkdirs()
+								if (writtenFile == null) {
+									info("New file [$size] written for \"$storePath\" at \"${requestedPath.absolutePath}\"")
+									var remainder = size
+									while (remainder > 0) {
+										val block = min(remainder.toInt(), 65536)
+										requestedPath.writeBytes(sock.inputStream.readNBytes(block))
+										remainder -= block
+									}
+									writtenFile = requestedPath
+								} else {
+									debug(
+										"File [$size] for \"$storePath\" copied to " +
+												"\"${requestedPath.absolutePath}\" from \"${writtenFile.absolutePath}\""
+									)
+									writtenFile.copyTo(requestedPath)
+								}
+							}
+							HTTPResponse(204, request.version)
+								.write(sock.outputStream)
+						}
+					}
+				}
+			} catch (e: SSLException) {
+				warn("SSL failure encountered; ${e.localizedMessage}")
+				sock.close()
+			} catch (e: IOException) {
+				warn("IO failure encountered; ${e.localizedMessage}")
+				sock.close()
+			}
+		}
+	}
 }
