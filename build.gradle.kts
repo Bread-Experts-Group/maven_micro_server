e709cdb5bc908e0eb0fc44b54d0b74bf9fc556cd
ATPStorages <tcgytsc@gmail.com>
2025-04-06 02:02:28 -0500

    Bump version 1.3.8, server update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "bread_experts_group"
-version = "1.3.7"
+version = "1.3.8"
 
 repositories {
 	mavenCentral()
@@ -20,7 +20,7 @@ repositories {
 dependencies {
 	testImplementation(kotlin("test"))
 	implementation(kotlin("reflect"))
-	implementation("bread_experts_group:bread_server_lib-code:1.6.3")
+	implementation("bread_experts_group:bread_server_lib-code:1.6.4")
 }
 
 tasks.test {
