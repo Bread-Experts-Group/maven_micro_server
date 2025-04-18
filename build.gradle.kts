20f813d7c60aed4c85ea11ffcf2a1022f3be4387
ATPStorages <tcgytsc@gmail.com>
2025-04-18 18:19:08 -0500

    Update BSL/SS to fix thread leak

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "org.bread_experts_group"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
 	mavenCentral()
@@ -20,8 +20,8 @@ repositories {
 dependencies {
 	testImplementation(kotlin("test"))
 	implementation(kotlin("reflect"))
-	implementation("org.bread_experts_group:bread_server_lib-code:1.0.0")
-	implementation("org.bread_experts_group:static_server-code:1.0.0")
+	implementation("org.bread_experts_group:bread_server_lib-code:1.1.0")
+	implementation("org.bread_experts_group:static_server-code:1.1.0")
 }
 
 tasks.test {
